39bf5332255c74e39ff3b3ce12898907f66af829
AP Java Period 5 <apjavaperiod5@AFVFX5003JC9H.local>
2022-10-03 12:08:22 -0400

    added favorite movie

---
+++ 01-favorite-movie/FavoriteMovie.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class FavoriteMovie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FavoriteMovie
+{
+    public static void main(String[] args) {
+    System.out.println("My favorite movie is Rise of The Planet of the Apes.");
+    System.out.print("It is about an ape named Ceasar who is adopted by a scientist named Will who is trying to find the cure for Alzheimers.");
+    System.out.println(" Ceasar has been injected by Will's experimented Alzheimer drug which has caused him to be highly intelligent compared to other apes.");
+    System.out.print("Will then adopts Ceasar after people verifying his cure saying it's ineffective. Will sees Ceasar's high intelligence. Will takes care of Ceasar like a son, and is like his teacher. Teaching Ceasar sign language to communicate.");
+    System.out.println(" Overtime, Ceasar grows up and who later on turns into society's threat.");
+    }
+}
