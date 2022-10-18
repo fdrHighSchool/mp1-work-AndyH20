c7cecd182cd8b36fdefec279a33fb3be740bab31
AP Java Period 5 <apjavaperiod5@AFVFX5003JC9H.local>
2022-10-18 12:03:16 -0400

    capital letters added

--- 06-username/UserName.java
+++ 06-username/UserName.java
@@ -30,10 +30,11 @@ public class UserName {
         System.out.println(getInital(firstName) + (lastName) + (fav) + "@schools.nyc.gov");
     }
 
-
+generatePassword(8);
     s.close();
   } // end main method
 
+  
   /*
    * Name: getInital
    * Purpose: send back the first character (inital) of a name
@@ -43,5 +44,30 @@ public class UserName {
   public static String getInital(String n) {
     return n.substring(0, 1);
 } // end initialize method
+
+
+public static String generatePassword(int length) {
+    String password = "";
+    int ca = (int)(Math.random() * (90-65+1) + 65);
+    char cap = (char)(ca);
+    System.out.println(cap + " " + cap);
+    
+    //generate a random number 
+    //65-90 cap
+    //33-47 special symbols
+    //97-122 ower case
+    
+    //convert integer to char c =(char)i;
+    
+    //add the char to the password string
+    
+    return (password);
+} //end pass
+  /*
+   * Name: generatePassword
+   * Purpose: send back a random String of numbers and upper/lower letters
+   * Input: length of password (int)
+   * Return: String
+   */
 
 } // end class
